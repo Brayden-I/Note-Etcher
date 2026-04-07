75f8856e9faa49b27a46ce48df8e9aa1eab55328
Brayden-I <imadabrayden1@gmail.com>
2026-04-07 15:22:33 -0500

    Made custom cursor and scenes enum

--- Note-Etcher/Game1.cs
+++ Note-Etcher/Game1.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace Note_Etcher;
 
+enum Scenes
+{
+    MAINMENU,
+    SETTINGS
+}
 public class Game1 : Game
 {
     private GraphicsDeviceManager _graphics;
     private SpriteBatch _spriteBatch;
+    private MouseCursor _cursor;
+    private Scenes ActiveScene;
+    
+    bool SpaceKeyPressed = false;
 
     public Game1()
     {
         _graphics = new GraphicsDeviceManager(this);
         Content.RootDirectory = "Content";
         IsMouseVisible = true;
+        ActiveScene = Scenes.MAINMENU;
     }
 
     protected override void Initialize()
@@ -27,24 +38,40 @@ public class Game1 : Game
     {
         _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-        // TODO: use this.Content to load your game content here
+        var cursorTexture = Content.Load<Texture2D>("Cursors/cursor_none");
+        _cursor = MouseCursor.FromTexture2D(cursorTexture, 0, 0);
+
     }
 
     protected override void Update(GameTime gameTime)
     {
+        Mouse.SetCursor(_cursor);
         if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
             Exit();
-
-        // TODO: Add your update logic here
+        switch (ActiveScene)
+        {
+            case  Scenes.MAINMENU:
+                // TODO: Add main menu logic
+                break;
+            case  Scenes.SETTINGS:
+                // TODO: Add Settings menu logic
+                break;
+        }
 
         base.Update(gameTime);
     }
 
     protected override void Draw(GameTime gameTime)
     {
-        GraphicsDevice.Clear(Color.CornflowerBlue);
-
-        // TODO: Add your drawing code here
+        if (SpaceKeyPressed)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+        }
+        else
+        {
+            GraphicsDevice.Clear(Color.DarkRed);
+        }
+        
 
         base.Draw(gameTime);
     }
