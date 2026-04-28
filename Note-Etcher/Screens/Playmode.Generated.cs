001541605fbce1596d26b862b84f4c39cacec5d7
Imada <bi1012440@otc.edu>
2026-04-28 09:24:14 -0500

    Finished menus

--- Note-Etcher/Screens/Playmode.Generated.cs
+++ Note-Etcher/Screens/Playmode.Generated.cs
@@ -7,6 +7,7 @@ using GumRuntime;
 using MonoGameGum;
 using MonoGameGum.GueDeriving;
 using Note_Etcher.Components.Controls;
+using Note_Etcher.Components.NoteEtcherComponents;
 using RenderingLibrary.Graphics;
 using System.Linq;
 namespace Note_Etcher.Screens;
@@ -37,10 +38,13 @@ if(element == null) throw new System.InvalidOperationException("Could not find a
             return gue;
         });
     }
+    public Album AlbumInstance { get; protected set; }
+    public Album AlbumInstance1 { get; protected set; }
+    public Album AlbumInstance2 { get; protected set; }
     public TextRuntime Title { get; protected set; }
     public TextRuntime Header { get; protected set; }
-    public ButtonStandard BackButton { get; protected set; }
     public ContainerRuntime AlbumContainer { get; protected set; }
+    public ButtonStandard BackButton { get; protected set; }
 
     public Playmode(InteractiveGue visual) : base(visual)
     {
@@ -54,10 +58,13 @@ if(element == null) throw new System.InvalidOperationException("Could not find a
     protected override void ReactToVisualChanged()
     {
         base.ReactToVisualChanged();
+        AlbumInstance = global::Gum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Album>(this.Visual,"AlbumInstance");
+        AlbumInstance1 = global::Gum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Album>(this.Visual,"AlbumInstance1");
+        AlbumInstance2 = global::Gum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<Album>(this.Visual,"AlbumInstance2");
         Title = this.Visual?.GetGraphicalUiElementByName("Title") as global::MonoGameGum.GueDeriving.TextRuntime;
         Header = this.Visual?.GetGraphicalUiElementByName("Header") as global::MonoGameGum.GueDeriving.TextRuntime;
-        BackButton = global::Gum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<ButtonStandard>(this.Visual,"BackButton");
         AlbumContainer = this.Visual?.GetGraphicalUiElementByName("AlbumContainer") as global::MonoGameGum.GueDeriving.ContainerRuntime;
+        BackButton = global::Gum.Forms.GraphicalUiElementFormsExtensions.TryGetFrameworkElementByName<ButtonStandard>(this.Visual,"BackButton");
         CustomInitialize();
     }
     //Not assigning variables because Object Instantiation Type is set to By Name rather than Fully In Code
