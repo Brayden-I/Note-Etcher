e2c6f65b7964be1fc33bcfe750f056b808e96ff5
Brayden-I <bi1012440@otc.edu>
2026-05-06 22:09:58 -0500

    Added charting

--- Note-Etcher/Services/Charts/Chart.cs
+++ Note-Etcher/Services/Charts/Chart.cs
@@ -6,4 +6,12 @@ public class Chart
 {
     public int LaneCount { get; set; } = 4;
     public List<ChartRow> Rows { get; set; } = new();
+
+    public Queue<ChartRow> ToPlaybackQueue()
+    {
+        var queue = new Queue<ChartRow>();
+        foreach (var row in Rows)
+            queue.Enqueue(row);
+        return queue;
+    }
 }
